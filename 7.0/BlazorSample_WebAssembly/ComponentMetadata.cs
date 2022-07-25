819773a822c463cafd86eb7478c9bed247258301
guardrex <1622880+guardrex@users.noreply.github.com>
2022-07-25 07:42:35 -0500

    Add 7.0 snippet sample apps

---
+++ 7.0/BlazorSample_WebAssembly/ComponentMetadata.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorSample
+{
+    public class ComponentMetadata
+    {
+        public string? Name { get; set; }
+        public Dictionary<string, object> Parameters { get; set; } =
+            new Dictionary<string, object>();
+    }
+}
---
+++ 7.0/BlazorSample_WebAssembly/NotifierService.cs
@@ -0,0 +1,12 @@
+﻿public class NotifierService
+{
+    public async Task Update(string key, int value)
+    {
+        if (Notify != null)
+        {
+            await Notify.Invoke(key, value);
+        }
+    }
+
+    public event Func<string, int, Task>? Notify;
+}
