0d4dc149c41fa8c4a58212169e24dfa12e9692b8
guardrex <1622880+guardrex@users.noreply.github.com>
2022-07-26 06:14:13 -0500

    Add 7.0 sample apps

---
+++ 7.0/BlazorWebAssemblyScopesLogger/CustomLoggerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+public class CustomLoggerConfiguration
+{
+    public int EventId { get; set; }
+
+    public Dictionary<LogLevel, LogFormat> LogLevels { get; set; } = 
+        new()
+        {
+            [LogLevel.Information] = LogFormat.Short,
+            [LogLevel.Warning] = LogFormat.Short,
+            [LogLevel.Error] = LogFormat.Long
+        };
+
+    public enum LogFormat
+    {
+        Short,
+        Long
+    }
+
+    public bool IncludeScopes { get; set; } = false;
+}
---
+++ 7.0/BlazorWebAssemblySignalRApp/Server/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorWebAssemblySignalRApp.Server.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+	opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+		new[] { "application/octet-stream" });
+});
+
+var app = builder.Build();
+
+#region snippet_Configure
+app.UseResponseCompression();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapHub<ChatHub>("/chathub");
+app.MapFallbackToFile("index.html");
+
+app.Run();
+#endregion
