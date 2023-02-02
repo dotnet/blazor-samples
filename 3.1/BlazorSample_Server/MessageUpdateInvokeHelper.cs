ce66f3729864509b3aa3b831a500ef93642fdc64
guardrex <1622880+guardrex@users.noreply.github.com>
2023-02-02 06:11:53 -0600

    Update JS interop examples

--- 3.1/BlazorSample_Server/MessageUpdateInvokeHelper.cs
+++ 3.1/BlazorSample_Server/MessageUpdateInvokeHelper.cs
@@ -10,7 +10,7 @@ public class MessageUpdateInvokeHelper
         this.action = action;
     }
 
-    [JSInvokable("BlazorSample")]
+    [JSInvokable]
     public void UpdateMessageCaller()
     {
         action.Invoke();
