2f0bc8c7c35619fd5ab9b08be6287a7a3f272a8d
guardrex <1622880+guardrex@users.noreply.github.com>
2023-05-09 07:06:29 -0400

    Add partial class examples

---
+++ 5.0/BlazorSample_WebAssembly/Pages/index/CounterPartialClass.razor.cs
@@ -0,0 +1,12 @@
+namespace BlazorSample.Pages.index
+{
+    public partial class CounterPartialClass
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
