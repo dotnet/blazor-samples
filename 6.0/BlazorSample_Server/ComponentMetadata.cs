2e608509f6d645e8292cd4d713d0fc02d680ff1e
guardrex <1622880+guardrex@users.noreply.github.com>
2023-07-25 16:22:08 -0400

    Add sample code

--- 6.0/BlazorSample_Server/ComponentMetadata.cs
+++ 6.0/BlazorSample_Server/ComponentMetadata.cs
@@ -1,6 +1,4 @@
-﻿namespace BlazorSample;
-
-public class ComponentMetadata
+﻿public class ComponentMetadata
 {
     public string? Name { get; set; }
     public Dictionary<string, object> Parameters { get; set; } =
