6eb199db57f285cc83d5f7b8fe087e3e5527b886
Luke Latham <1622880+guardrex@users.noreply.github.com>
2023-07-26 14:19:05 -0400

    Add code samples (#96)

--- 6.0/BlazorSample_WebAssembly/Program.cs
+++ 6.0/BlazorSample_WebAssembly/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration.Memory;
 using BlazorSample;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,5 +9,21 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<NotifierService>();
+
+#region snippet1
+var vehicleData = new Dictionary<string, string?>()
+{
+    { "color", "blue" },
+    { "type", "car" },
+    { "wheels:count", "3" },
+    { "wheels:brand", "Blazin" },
+    { "wheels:brand:type", "rally" },
+    { "wheels:year", "2008" },
+};
+
+var memoryConfig = new MemoryConfigurationSource { InitialData = vehicleData };
+
+builder.Configuration.Add(memoryConfig);
+#endregion
 
 await builder.Build().RunAsync();
