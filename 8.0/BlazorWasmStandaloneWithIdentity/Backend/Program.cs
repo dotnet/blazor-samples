769b5d4e61d991c9edf730cfb58a2e130b4b2e52
Luke Latham <1622880+guardrex@users.noreply.github.com>
2023-11-06 10:34:12 -0500

    Blazor standalone WASM Identity updates (#113)

--- 8.0/BlazorWasmStandaloneWithIdentity/Backend/Program.cs
+++ 8.0/BlazorWasmStandaloneWithIdentity/Backend/Program.cs
@@ -1,7 +1,7 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
