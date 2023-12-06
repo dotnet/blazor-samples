b5f219152d80c1983b87c19008a95e585a18e4bb
guardrex <1622880+guardrex@users.noreply.github.com>
2023-12-06 07:28:12 -0500

    Updates

--- 8.0/BlazorSample_BlazorWebApp/BlazorSample.Client/BlazorRocksBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace BlazorSample.Client;
-
-public class BlazorRocksBase : ComponentBase
-{
-    public string BlazorRocksText { get; set; } =
-        "Blazor rocks the browser!";
-}
