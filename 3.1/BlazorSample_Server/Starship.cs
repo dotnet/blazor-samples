2dd76424245ed1d56b11f904ac56aeeefa6b7cac
Luke Latham <1622880+guardrex@users.noreply.github.com>
2024-01-05 08:28:04 -0500

    Update model property name (#156)

--- 3.1/BlazorSample_Server/Starship.cs
+++ 3.1/BlazorSample_Server/Starship.cs
@@ -5,7 +5,7 @@ public class Starship
 {
     [Required]
     [StringLength(16, ErrorMessage = "Identifier too long (16 character limit).")]
-    public string Identifier { get; set; }
+    public string Id { get; set; }
 
     public string Description { get; set; }
 
