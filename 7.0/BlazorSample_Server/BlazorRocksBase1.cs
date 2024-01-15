df1e84bed4e0df9d13e11f6d2040376338486f24
guardrex <1622880+guardrex@users.noreply.github.com>
2024-01-15 07:26:45 -0500

    New base class examples

---
+++ 7.0/BlazorSample_Server/BlazorRocksBase1.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorSample;
+
+public class BlazorRocksBase1 : ComponentBase
+{
+    public string BlazorRocksText { get; set; } =
+        "Blazor rocks the browser!";
+}
