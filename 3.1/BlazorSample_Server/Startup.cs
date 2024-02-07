431ba42b5b2d46076100537f327a4e8c56fba3c8
guardrex <1622880+guardrex@users.noreply.github.com>
2024-02-07 09:29:10 -0500

    Move transient disposables code

--- 3.1/BlazorSample_Server/Startup.cs
+++ 3.1/BlazorSample_Server/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorSample.Data;
+using BlazorSample.Services;
 
 namespace BlazorSample
 {
