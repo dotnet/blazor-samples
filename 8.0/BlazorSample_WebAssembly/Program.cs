1227d4d925a6b63846c6700a52af397420a66ebb
guardrex <1622880+guardrex@users.noreply.github.com>
2024-02-09 08:45:01 -0500

    Standardize component params

--- 8.0/BlazorSample_WebAssembly/Program.cs
+++ 8.0/BlazorSample_WebAssembly/Program.cs
@@ -4,15 +4,12 @@ using Microsoft.Extensions.Configuration.Memory;
 using BlazorSample;
 using static BlazorSample.Pages.ProductDetails;
 using static BlazorSample.Pages.TheSunmakers;
-using BlazorSample.Services;
+//using BlazorSample.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.DetectIncorrectUsageOfTransients();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddSingleton<NotifierService>();
 builder.Services.AddSingleton<TimerService>();
 builder.Services.AddSingleton<IDataAccess, DataAccess>();
@@ -20,7 +17,8 @@ builder.Services.AddSingleton<IProductRepository, ProductRepository>();
 builder.Services.AddCascadingValue(sp => new Dalek { Units = 123 });
 builder.Services.AddCascadingValue("AlphaGroup", sp => new Dalek { Units = 456 });
 builder.Services.AddTransient<SaladChef>();
-builder.Services.AddTransient<TransientDisposableService>();
+//builder.DetectIncorrectUsageOfTransients();
+//builder.Services.AddTransient<TransientDisposableService>();
 
 var vehicleData = new Dictionary<string, string?>()
 {
@@ -37,7 +35,5 @@ var memoryConfig = new MemoryConfigurationSource { InitialData = vehicleData };
 builder.Configuration.Add(memoryConfig);
 
 var host = builder.Build();
-
-host.EnableTransientDisposableDetection();
-
+//host.EnableTransientDisposableDetection();
 await host.RunAsync();
