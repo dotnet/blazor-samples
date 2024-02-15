5cdf6ddff8bb734314d6125e20f6ca5b923e8d5c
guardrex <1622880+guardrex@users.noreply.github.com>
2024-02-15 12:07:03 -0500

    Update adoption of cascading auth state

--- 8.0/BlazorWebAppOidcBff/BlazorWebAppOidc.Client/Program.cs
+++ 8.0/BlazorWebAppOidcBff/BlazorWebAppOidc.Client/Program.cs
@@ -6,6 +6,7 @@ using BlazorWebAppOidc.Client.Weather;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
 builder.Services.AddHttpClient<IWeatherForecaster, ClientWeatherForecaster>(httpClient =>
