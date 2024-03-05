96ad772f9c90deba1af1fd3699a8e39a1e6fcd8d
Luke Latham <1622880+guardrex@users.noreply.github.com>
2024-03-05 11:31:17 -0500

    Add call web API (weather) sample (#226)

---
+++ 8.0/BlazorWebAppCallWebApi_Weather/BlazorApp/Program.cs
@@ -0,0 +1,42 @@
+using BlazorApp;
+using BlazorApp.Client.Services;
+using BlazorApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+// Register the server-side weather service
+builder.Services.AddSingleton<IWeatherService, ServerWeatherService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorApp.Client._Imports).Assembly);
+
+// Minimal API for obtaining weather data from /api/weather
+app.MapGet("/api/weather", (IWeatherService weatherService) => weatherService.GetWeather());
+
+app.Run();
