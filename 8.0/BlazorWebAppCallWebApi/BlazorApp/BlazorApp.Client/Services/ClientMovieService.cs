98d641b7959eb97dc4ba1d7a6888f944d3177eaa
Luke Latham <1622880+guardrex@users.noreply.github.com>
2024-03-07 12:57:55 -0500

    Drop the prop (#229)

--- 8.0/BlazorWebAppCallWebApi/BlazorApp/BlazorApp.Client/Services/ClientMovieService.cs
+++ 8.0/BlazorWebAppCallWebApi/BlazorApp/BlazorApp.Client/Services/ClientMovieService.cs
@@ -1,40 +1,28 @@
-using Microsoft.AspNetCore.Components;
 using BlazorApp.Client.Models;
 using System.Net.Http.Json;
 
 namespace BlazorApp.Client.Services;
 
-public class ClientMovieService(HttpClient httpClient) : IMovieService
+public class ClientMovieService(HttpClient http) : IMovieService
 {
-    private HttpClient Http { get; set; } = httpClient;
-
     public async Task<Movie[]> GetMoviesAsync(bool watchedMovies)
     {
         var requestUri = watchedMovies ? $"movies/watched" : "movies";
-        return await Http.GetFromJsonAsync<Movie[]>(requestUri) ?? [];
+        return await http.GetFromJsonAsync<Movie[]>(requestUri) ?? [];
     }
 
     public async Task PostMovieAsync(Movie movie)
     {
-        if (Http is not null)
-        {
-            await Http.PostAsJsonAsync("movies", movie);
-        }
+        await http.PostAsJsonAsync("movies", movie);
     }
 
     public async Task PutMovieAsync(long id, Movie movie)
     {
-        if (Http is not null)
-        {
-            await Http.PutAsJsonAsync($"movies/{id}", movie);
-        }
+        await http.PutAsJsonAsync($"movies/{id}", movie);
     }
 
     public async Task DeleteMovieAsync(long id)
     {
-        if (Http is not null)
-        {
-            await Http.DeleteAsync($"movies/{id}");
-        }
+        await http.DeleteAsync($"movies/{id}");
     }
 }
