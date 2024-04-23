85d0762729f8148fdc7a50a0ef11392eee1b661e
Luke Latham <1622880+guardrex@users.noreply.github.com>
2024-04-23 07:43:10 -0400

    MAUI + BWA sample updates (#271)

--- 8.0/MAUIBlazorWeb/MAUIBlazorWeb.MAUI/Platforms/Tizen/Main.cs
+++ 8.0/MAUIBlazorWeb/MAUIBlazorWeb.MAUI/Platforms/Tizen/Main.cs
@@ -2,7 +2,7 @@ using System;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 
-namespace MAUIBlazorWeb.MAUI
+namespace MauiBlazorWeb.Maui
 {
     internal class Program : MauiApplication
     {
