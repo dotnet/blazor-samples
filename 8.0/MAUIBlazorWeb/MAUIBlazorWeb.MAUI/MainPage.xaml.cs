cd14551fb62fa2fc17335164d69e947c768c7ef4
guardrex <1622880+guardrex@users.noreply.github.com>
2024-04-23 05:34:57 -0400

    Add MAUIBlazorWeb sample app

---
+++ 8.0/MAUIBlazorWeb/MAUIBlazorWeb.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIBlazorWeb.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
