0ebb086b8054550c156ae0e49c7d7acb0a341f4e
guardrex <1622880+guardrex@users.noreply.github.com>
2024-04-27 06:13:06 -0400

    Break long lines in InteractiveRenderSettings

--- 8.0/MauiBlazorWeb/MauiBlazorWeb.Shared/InteractiveRenderSettings.cs
+++ 8.0/MauiBlazorWeb/MauiBlazorWeb.Shared/InteractiveRenderSettings.cs
@@ -5,9 +5,12 @@ namespace MauiBlazorWeb.Shared;
 
 public static class InteractiveRenderSettings
 {
-    public static IComponentRenderMode? InteractiveServer { get; set; } = RenderMode.InteractiveServer;
-    public static IComponentRenderMode? InteractiveAuto { get; set; } = RenderMode.InteractiveAuto;
-    public static IComponentRenderMode? InteractiveWebAssembly { get; set; } = RenderMode.InteractiveWebAssembly;
+    public static IComponentRenderMode? InteractiveServer { get; set; } = 
+        RenderMode.InteractiveServer;
+    public static IComponentRenderMode? InteractiveAuto { get; set; } = 
+        RenderMode.InteractiveAuto;
+    public static IComponentRenderMode? InteractiveWebAssembly { get; set; } = 
+        RenderMode.InteractiveWebAssembly;
 
     public static void ConfigureBlazorHybridRenderModes()
     {
