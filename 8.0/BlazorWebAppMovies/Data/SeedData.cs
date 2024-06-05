149a8929c715e83e4f431e402237d60676af2ea8
guardrex <1622880+guardrex@users.noreply.github.com>
2024-06-05 10:17:49 -0400

    Movie tutorial sample updates

--- 8.0/BlazorWebAppMovies/Data/SeedData.cs
+++ 8.0/BlazorWebAppMovies/Data/SeedData.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using BlazorWebAppMovies.Models;
-using Humanizer;
-using System;
 
 namespace BlazorWebAppMovies.Data
 {
@@ -24,51 +22,6 @@ namespace BlazorWebAppMovies.Data
                 return;
             }
 
-            /* The following is used up to Part 7 of
-            the tutorial series.
-
-            context.Movie.AddRange(
-                new Movie
-                {
-                    Title = "Mad Max",
-                    ReleaseDate = DateTime.Parse("1979-4-12"),
-                    Genre = "Sci-fi (Cyberpunk)",
-                    Price = 2.51M,
-                },
-                new Movie
-                {
-                    Title = "The Road Warrior",
-                    ReleaseDate = DateTime.Parse("1981-12-24"),
-                    Genre = "Sci-fi (Cyberpunk)",
-                    Price = 2.78M,
-                },
-                new Movie
-                {
-                    Title = "Mad Max: Beyond Thunderdome",
-                    ReleaseDate = DateTime.Parse("1985-7-10"),
-                    Genre = "Sci-fi (Cyberpunk)",
-                    Price = 3.55M,
-                },
-                new Movie
-                {
-                    Title = "Mad Max: Fury Road",
-                    ReleaseDate = DateTime.Parse("2015-5-15"),
-                    Genre = "Sci-fi (Cyberpunk)",
-                    Price = 8.43M,
-                },
-                new Movie
-                {
-                    Title = "Furiosa: A Mad Max Saga",
-                    ReleaseDate = DateTime.Parse("2024-5-24"),
-                    Genre = "Sci-fi (Cyberpunk)",
-                    Price = 13.49M,
-                }
-            );
-
-            */
-            /* The following is used in Parts 7 and 8, the
-            last two parts of the tutorial series.
-            */
             context.Movie.AddRange(
                 new Movie
                 {
