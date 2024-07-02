c913b21ebd90741c8caee544549f55c37330dff6
guardrex <1622880+guardrex@users.noreply.github.com>
2024-07-02 11:49:56 -0400

    Update movie tutorial sample app

--- 8.0/BlazorWebAppMovies/Models/Movie.cs
+++ 8.0/BlazorWebAppMovies/Models/Movie.cs
@@ -2,27 +2,25 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Humanizer;
 
-namespace BlazorWebAppMovies.Models
+namespace BlazorWebAppMovies.Models;
+
+public class Movie
 {
-    public class Movie
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [Required, StringLength(60, MinimumLength = 3)]
-        public string? Title { get; set; }
+    [Required, StringLength(60, MinimumLength = 3)]
+    public string? Title { get; set; }
 
-        [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
+    public DateOnly ReleaseDate { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$"), Required, StringLength(30)]
-        public string? Genre { get; set; }
+    [RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$"), Required, StringLength(30)]
+    public string? Genre { get; set; }
 
-        [Range(0, 100)]
-        [DataType(DataType.Currency)]
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal Price { get; set; }
+    [Range(0, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
 
-        [RegularExpression(@"^(G|PG|PG-13|R|NC-17)$"), Required]
-        public string? Rating { get; set; }
-    }
+    [RegularExpression(@"^(G|PG|PG-13|R|NC-17)$"), Required]
+    public string? Rating { get; set; }
 }
