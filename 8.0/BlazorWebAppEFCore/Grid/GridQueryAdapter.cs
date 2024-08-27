b5f5d15fdd3ca95b919484b8e1c09d57af5d5ee9
Luke Latham <1622880+guardrex@users.noreply.github.com>
2024-08-27 06:16:45 -0400

    Updates

--- 8.0/BlazorWebAppEFCore/Grid/GridQueryAdapter.cs
+++ 8.0/BlazorWebAppEFCore/Grid/GridQueryAdapter.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using System.Linq.Expressions;
-using BlazorWebAppEFCore.Data;
 using Microsoft.EntityFrameworkCore;
+using BlazorWebAppEFCore.Data;
 
 namespace BlazorWebAppEFCore.Grid;
 
