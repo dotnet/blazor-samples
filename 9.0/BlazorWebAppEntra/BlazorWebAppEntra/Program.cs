37dd971913a73f0527d4dcff6337c9a6b0606e8c
Stephen Halter <halter73@gmail.com>
2024-10-03 08:09:27 -0700

    Add Blazor Web App using 9.0.0-rc.1 Template
    - dotnet new blazor --interactivity Auto --all-interactive --auth None

---
+++ 9.0/BlazorWebAppEntra/BlazorWebAppEntra/Program.cs
@@ -0,0 +1,36 @@
+using BlazorWebAppEntra.Client.Pages;
+using BlazorWebAppEntra.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorWebAppEntra.Client._Imports).Assembly);
+
+app.Run();
