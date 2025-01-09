37649d95124d955081d2b930885c4cb991ab7bab
Luke Latham <1622880+guardrex@users.noreply.github.com>
2025-01-09 12:51:54 -0500

    XrefGen publishing (#429)

--- 9.0/BlazorWebAssemblyXrefGenerator/Program.cs
+++ 9.0/BlazorWebAssemblyXrefGenerator/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using XrefGenBlazor;
+using BlazorWebAssemblyXrefGenerator;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
