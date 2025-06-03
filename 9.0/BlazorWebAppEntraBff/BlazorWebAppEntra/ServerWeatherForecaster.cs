ecd0d34e348f922d54c2bfcee9a5e9bcfdeafe0e
Emmanuel <punky_casinotomo@hotmail.com>
2025-06-03 23:12:53 +0200

    Update ServerWeatherForecaster.cs
    when you inject this Class on the Program.cs you are passing a httpClient , and not a downstreamApi, therefore i change he implementation to use a httpClient, otherwise change the way its bieng inyected to builder.Services.AddScoped<IUserProfileProvider, ServerUserProfile>();

--- 9.0/BlazorWebAppEntraBff/BlazorWebAppEntra/ServerWeatherForecaster.cs
+++ 9.0/BlazorWebAppEntraBff/BlazorWebAppEntra/ServerWeatherForecaster.cs
@@ -3,17 +3,36 @@ using Microsoft.Identity.Abstractions;
 
 namespace BlazorWebAppEntra;
 
-internal sealed class ServerWeatherForecaster(IDownstreamApi downstreamApi) : IWeatherForecaster
+internal sealed class ServerWeatherForecaster : IWeatherForecaster
 {
+    private readonly HttpClient _httpClient;
+    private readonly ITokenAcquisition _tokenAcquisition;
+    private readonly IConfiguration _configuration;
+
+    public ServerWeatherForecaster(
+        HttpClient httpClient,
+        ITokenAcquisition tokenAcquisition,
+        IConfiguration configuration)
+    {
+        this._httpClient = httpClient;
+        this._tokenAcquisition = tokenAcquisition;
+        this._configuration = configuration;
+    }
+
     public async Task<IEnumerable<WeatherForecast>> GetWeatherForecastAsync()
     {
-        using var response = await downstreamApi.CallApiForUserAsync("DownstreamApi",
-            options =>
-            {
-                options.RelativePath = "/weather-forecast";
-            });
+       
+        var scopes = _configuration.GetSection("DownstreamApi:Scopes").Get<string[]>();
+        if (scopes == null || scopes.Length == 0)
+            throw new InvalidOperationException("No downstream API scopes configured!");
+
+        var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+
+         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+        var response = await _httpClient.GetAsync("/weather-forecast");
+        response.EnsureSuccessStatusCode();
 
         return await response.Content.ReadFromJsonAsync<WeatherForecast[]>() ??
             throw new IOException("No weather forecast!");
     }
-}
