12b0ebf695ea4a8f335611620d8295585b2e9506
Luke Latham <1622880+guardrex@users.noreply.github.com>
2025-07-21 06:43:34 -0400

    Update Swashbuckle link

--- 9.0/MauiBlazorWebIdentity/MauiBlazorWeb/MauiBlazorWeb.Web/Program.cs
+++ 9.0/MauiBlazorWebIdentity/MauiBlazorWeb/MauiBlazorWeb.Web/Program.cs
@@ -41,7 +41,8 @@ builder.Services.AddIdentityApiEndpoints<ApplicationUser>(options => options.Sig
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at 
+// https://learn.microsoft.com/aspnet/core/tutorials/getting-started-with-swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
