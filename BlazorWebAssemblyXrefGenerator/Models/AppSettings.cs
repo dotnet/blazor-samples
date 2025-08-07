ae9a003425dc7cc3f443aa7dd121624c9231ad35
Ben Hopkins <ben@hopkins.tech>
2025-08-07 23:23:15 +0100

    Created AppSettings in xref-generator

---
+++ BlazorWebAssemblyXrefGenerator/Models/AppSettings.cs
@@ -0,0 +1,8 @@
+namespace BlazorWebAssemblyXrefGenerator.Models;
+
+public class AppSettings
+{
+    public string MicrosoftLearnUrl { get; set; } = string.Empty;
+
+    public List<string> DotNetVersions { get; set; } = [];
+}
--- BlazorWebAssemblyXrefGenerator/Program.cs
+++ BlazorWebAssemblyXrefGenerator/Program.cs
@@ -1,13 +1,18 @@
+using BlazorWebAssemblyXrefGenerator;
+using BlazorWebAssemblyXrefGenerator.Models;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorWebAssemblyXrefGenerator;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var appSettings = new AppSettings();
+builder.Configuration.Bind(appSettings);
+builder.Services.AddSingleton(appSettings);
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddHttpClient("APIClient", client => client.BaseAddress = new Uri("https://learn.microsoft.com"));
+builder.Services.AddHttpClient("APIClient", client => client.BaseAddress = new Uri(appSettings.MicrosoftLearnUrl));
 
 await builder.Build().RunAsync();
