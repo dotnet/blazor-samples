4642bb0946189fbf7c2b84c10ca8665e91a740c1
Luke Latham <1622880+guardrex@users.noreply.github.com>
2025-11-13 13:08:17 -0500

    Add 10.0 Blazor Web App Movies tutorial sample app (#628)

---
+++ 10.0/BlazorWebAppMovies/Data/BlazorWebAppMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorWebAppMovies.Models;
+
+namespace BlazorWebAppMovies.Data
+{
+    public class BlazorWebAppMoviesContext : DbContext
+    {
+        public BlazorWebAppMoviesContext (DbContextOptions<BlazorWebAppMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorWebAppMovies.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ 10.0/BlazorWebAppMovies/Migrations/20251113175430_AddRatingField.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWebAppMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRatingField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "R");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+        }
+    }
+}
