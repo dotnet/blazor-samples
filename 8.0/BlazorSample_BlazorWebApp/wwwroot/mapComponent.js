62203766ce79c3c6628b91846333196ac34cc026
guardrex <1622880+guardrex@users.noreply.github.com>
2026-04-06 17:25:55 -0400

    Updates

--- 8.0/BlazorSample_BlazorWebApp/wwwroot/mapComponent.js
+++ 8.0/BlazorSample_BlazorWebApp/wwwroot/mapComponent.js
@@ -1,6 +1,6 @@
 ﻿import 'https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js';
 
-mapboxgl.accessToken = '{MAPBOX_ACCESS_TOKEN}';
+mapboxgl.accessToken = '{MAPBOX ACCESS TOKEN}';
 
 export function addMapToElement(element) {
   return new mapboxgl.Map({
